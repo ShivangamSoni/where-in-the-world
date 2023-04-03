6bcd5e4b1bf78c6b9eb49fcdc9f5d25f8bef4450
Shivangam Soni <shivangamsoni99@gmail.com>
2023-04-03 20:15:56 +0530

    Add Alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { resolve } from "path";
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: [
+            {
+                find: "@features",
+                replacement: resolve(__dirname, "src", "features"),
+            },
+        ],
+    },
+});
